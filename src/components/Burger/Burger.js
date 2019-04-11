f8ae45f84999a90901c3cb3331161d337a2de01d
Raphael Neves <raphael.neves@utilus.nl>
2019-04-11 11:35:02 +0100

    Preparing ingredients to be added dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,13 +5,17 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
 
+    let burgerIngredients = Object.keys(props.ingredients)
+        .map(key => {
+            return [...Array(props.ingredients[key])].map((_, index) => {
+                return <BurgerIngredient key={key + index} type={key} />
+            });
+        });
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='top-bread' />
-            <BurgerIngredient type='salad' />
-            <BurgerIngredient type='bacon' />
-            <BurgerIngredient type='cheese' />
-            <BurgerIngredient type='meat' />
+            {burgerIngredients}
             <BurgerIngredient type='bottom-bread' />
         </div>
     );
