9c9bb933270eae8f2b372d6779a59360072abbac
Raphael Neves <raphael.neves@utilus.nl>
2019-04-11 11:00:05 +0100

    Creating BurgerBuilder component

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Aux from "../../hoc/Aux";
+
+class BurgerBuilder extends Component {
+    render() {
+        return (
+            <Aux>
+                <div>Dynamic Burger</div>
+                <div>Burger build controls</div>
+            </Aux>
+        );
+    };
+}
+export default BurgerBuilder;
